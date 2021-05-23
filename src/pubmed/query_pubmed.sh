71a708d9e6a05c1a21e45ee9d550e008c4bbaf98
Ben Berger <benjaminaberger94@gmail.com>
2021-05-23 16:07:43 -0400

    Fetched IDs for several MPNs.

--- src/pubmed/query_pubmed.sh
+++ src/pubmed/query_pubmed.sh
@@ -1,16 +1,28 @@
+#!/bin/bash
 
+# Define data path
+id_path=./data/raw/pubmed/ids
 
 # Pull MPN ids
-mpn_path=./data/raw/pubmed/mpn/mpn_ids.txt
-cancer_path=./data/raw/pubmed/cancer/cancer_ids.txt
-
-esearch -db pubmed -query "(Polycythemia Vera|Essential thrombocythemia|Primary myelofibrosis) & cancer[sb]" | efetch -format uid > $mpn_path
-
-# Count number of MPN IDs
-echo "MPN ID Count"
-wc -l $mpn_path
+echo "Pulling PV IDs"
+esearch -db pubmed -query "Polycythemia Vera" | efetch -format uid > $id_path/pv_ids.txt
+echo "Pulling ET IDs"
+esearch -db pubmed -query "Essential thrombocythemia" | efetch -format uid > $id_path/et_ids.txt
+echo "Pulling PMF IDs"
+esearch -db pubmed -query "Primary myelofibrosis" | efetch -format uid > $id_path/pmf_ids.txt
+echo "Pulling AML IDs"
+esearch -db pubmed -query "Acute myeloid leukemia" | efetch -format uid > $id_path/aml_ids.txt
+echo "Pulling CML IDs"
+esearch -db pubmed -query "Chronic myelogenous leukemia" | efetch -format uid > $id_path/cml_ids.txt
 
 # Pull all cancer ids
-esearch -db pubmed -query "cancer[sb]" | efetch -format uid > $cancer_path
-wc -l $cancer_path
-
+while [ -n "$1" ]; do # while loop starts
+     case "$1" in
+     -all_cancer) {
+        echo "Pulling all cancer IDs.";
+        esearch -db pubmed -query "cancer[sb]" | efetch -format uid > $id_path/cancer_ids.txt
+        };; # Message for -a option
+     esac
+     shift
+done
+echo "Finished!"
